ecfc66bc87ff5669edfa1d5563a30674ff012e11
Fabian Quijosaca <fabianq44@gmail.com>
2019-01-27 10:40:58 -0500

    adding comment to code

--- WaterJugRiddle.pl
+++ WaterJugRiddle.pl
@@ -2,18 +2,36 @@
 % Fecha: 01/01/2019
 
 %        The Water Jug Riddle by foqc
+
+% Goal
 meta(estado(4,_)).
-                  % CJG= amount of water in the large jar,   CJP= amount of water in the small jar
+
+                  % CJG= amount of water in the large jug,   CJP= amount of water in the small jug
                   % MJG= maximum capacity of the large jug,    MJP= maximum capacity of the small jug.
 
-posible(estado(CJG,CJP), estado(CJG1, CJP), MJG, _):- CJG>=0, CJG < MJG, CJG1 is MJG. % llena jarra gande
-posible(estado(CJG,CJP), estado(CJG, CJP1), _, MJP):- CJP>=0, CJP < MJP, CJP1 is MJP. % LLENA JARRA peque;a
-posible(estado(CJG,CJP),estado(CJG1,CJP), _, _):- CJG > 0, CJG1 is 0.% vaciar jarra grande
-posible(estado(CJG,CJP),estado(CJG,CJP1), _, _):- CJP > 0, CJP1 is 0.% vaciar jarra peque;a
-posible(estado(CJG,CJP),estado(CJG1,CJP1), _, MJP):- CJG > 0, CJP < MJP, CJG =< (MJP - CJP), CJG1 is 0, CJP1 is CJP + CJG.% pasar de jarra grande a peque;a cuando agua jarra grande es menor CJP
-posible(estado(CJG,CJP),estado(CJG1,CJP1), _, MJP):- CJG > 0, CJP < MJP, CJG > (MJP - CJP), CJG1 is CJG - (MJP - CJP), CJP1 is MJP.% pasar de jarra grande a peque;a cuando agua jarra grande es mayor CJP
-posible(estado(CJG,CJP),estado(CJG1,CJP1), MJG, _):- CJP > 0, CJG < MJG, CJP =< (MJG - CJG), CJP1 is 0, CJG1 is CJG + CJP .% pasar de jarra peque;a a grande cuando el agua CJP menor   agua CJG
-posible(estado(CJG,CJP),estado(CJG1,CJP1), MJG, _):- CJP > 0, CJG < MJG, CJP > (MJG - CJG), CJP1 is CJP - (MJG - CJG), CJG1 is MJG.% pasar de jarra peque;a a grande cuando CJP es mayor a lo que falta para llenar a la jarra grande
+%fill the large jug
+posible(estado(CJG,CJP), estado(CJG1, CJP), MJG, _):- CJG>=0, CJG < MJG, CJG1 is MJG.
+
+% fill the small jug
+posible(estado(CJG,CJP), estado(CJG, CJP1), _, MJP):- CJP>=0, CJP < MJP, CJP1 is MJP.
+
+% empty the large jug
+posible(estado(CJG,CJP),estado(CJG1,CJP), _, _):- CJG > 0, CJG1 is 0.
+
+% empty the small jug
+posible(estado(CJG,CJP),estado(CJG,CJP1), _, _):- CJP > 0, CJP1 is 0.
+
+% Pour the water from the large jug into the small jug and the amount of water in the large jug is LESS than the amount of water left to fill the small jug CJP
+posible(estado(CJG,CJP),estado(CJG1,CJP1), _, MJP):- CJG > 0, CJP < MJP, CJG =< (MJP - CJP), CJG1 is 0, CJP1 is CJP + CJG.
+
+% Pour the water from the large jug into the small jug and the amount of water in the large jug is MORE than the amount of water left to fill the small jug CJP
+posible(estado(CJG,CJP),estado(CJG1,CJP1), _, MJP):- CJG > 0, CJP < MJP, CJG > (MJP - CJP), CJG1 is CJG - (MJP - CJP), CJP1 is MJP.
+
+% Pour the water from the small jug into the large jug and the amount of water in the small jug is LESS than the amount of water left to fill the large jug CJG
+posible(estado(CJG,CJP),estado(CJG1,CJP1), MJG, _):- CJP > 0, CJG < MJG, CJP =< (MJG - CJG), CJP1 is 0, CJG1 is CJG + CJP .
+
+% Pour the water from the small jug into the large jug and the amount of water in the small jug is MORE than the amount of water left to fill the small jug CJG
+posible(estado(CJG,CJP),estado(CJG1,CJP1), MJG, _):- CJP > 0, CJG < MJG, CJP > (MJG - CJG), CJP1 is CJP - (MJG - CJG), CJG1 is MJG.
 
           /* COMPARE THE NEW STATE WITH THE PREVIOUS */
 miembro(X,[X|_]).
